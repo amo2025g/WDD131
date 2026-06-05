1c94dbf8209d7a18d578539eb7bdf1b8388c6d9a
Blossom cupid <aikeovaii@gmail.com>
2026-06-05 16:49:12 -0600

    Ok

--- mission/index.js
+++ mission/index.js
@@ -5,24 +5,27 @@ selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current == 'dark') {
-      document.body.style.backgroundColor = "#393939";
-      document.body.style.color = "#e8e9ed";
 
-      const text = document.querySelector("h5");
-      text.style.color = "#9bd5ff";
-      
-      logo.src = "img/byui-logo-white.png";
-      logo.alt = "BYUI Logo";
-      document.querySelector(".center").style.outlineColor = "#acacb8";
+    if (current == 'dark') {
+        document.body.style.backgroundColor = "#393939";
+        document.body.style.color = "#e8e9ed";
+
+        const text = document.querySelector("h5");
+        text.style.color = "#9bd5ff";
+
+        logo.src = "img/byui-logo-white.png";
+        logo.alt = "BYUI Logo";
+
+        document.querySelector(".center").style.outlineColor = "#acacb8";
+
     } else {
-      document.body.style.backgroundColor = "white";
-      document.body.style.color = "black";
-      
-      const text = document.querySelector("h5");
-      text.style.color = "#598ed4";
-      
-      logo.src = "img/byui-logo-blue.webp";
-      logo.alt = "BYUI Logo";
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+
+        const text = document.querySelector("h5");
+        text.style.color = "#598ed4";
+
+        logo.src = "img/byui-logo-blue.webp";
+        logo.alt = "BYUI Logo";
     }
- }
+}
