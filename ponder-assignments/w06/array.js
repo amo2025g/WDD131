bb2a94bb09a3f96075d692c1260b64c4abd67de2
Blossom cupid <aikeovaii@gmail.com>
2026-07-09 16:51:26 -0600

    array

--- ponder-assignments/w06/array.js
+++ ponder-assignments/w06/array.js
@@ -1,31 +1,116 @@
-function convert(grade) {
-    switch (grade){
-        case 'A':
-            points = 4;
-            break;
-        case 'B':
-            points = 3;
-            break;
-        case 'C':
-            points = 2;
-            break;
-        case 'D':
-            points = 1;
-            break;
-        case 'F':
-            points = 0;
-            break;
-        default:
-            alert('not a valid grade');
-    }
-    return points;
+//.forEach
+const steps = ['one', 'two', 'three'];
+
+//steps.forEach(function(item){
+//    console.log
+//})
+
+steps.forEach(showStep);
+function showStep(item){ // doesn't have to be item, as long as the parameter is the same everywhere
+    console.log(item);
 }
 
+
+
+
+//.map
+let myList = document.querySelector('#myList');
+const stepsHTML = steps.map(listTemplate);
+
+function listTemplate(item) {
+    return `<li>${item}</li>`; // will get returned back into stepsHtml and will be changed as list items
+};
+
+myList.innerHTML = stepsHTML.join('');
+
+let grades = ['A', 'B', 'C'];
+let points;
+
+let gpaPoints = grades.map(convert);
+
+function convert(grade) {
+   switch (grade){
+       case 'A':
+           points = 4;
+           break; // won't check other cases once found
+       case 'B':
+           points = 3;
+           break;
+       case 'C':
+           points = 2;
+           break;
+       case 'D':
+           points = 1;
+           break;
+       case 'F':
+           points = 0;
+           break;
+       default:
+           alert('not a valid grade');
+   }
+   return points;
+}
+console.log(gpaPoints);
+
+let totalPoints = gpaPoints.reduce(getTotal);
+
+function getTotal(total, item){
+    return total + item;
+}
+console.log(totalPoints);
+
+let gpaAverage = totalPoints/gpaPoints.length; // .length gets all values
+console.log(gpaAverage); 
+
+
+
+
+//.filter
 const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
 
+const shortWords = words.filter(function(word){
+    return word.length <= 6
+})
+console.log(shortWords);
+
+
+
+//.indexOf
+const myArray = [12, 34, 21, 54];
+const luckyNumber = 21;
+let luckyIndex = myArray.indexOf(luckyNumber);
+console.log(luckyIndex);
+
+
+
+
+//dynamic content
+let container = document.querySelector('#studentContainer');
 
 const students = [
-    {last: 'Andrus', first: 'Aaron'},
-    {last: 'Masa', first:'Manny'},
-    {last: 'Tanda', first: 'Tamanda'}
+    {
+        last: 'Andrus',
+        first: 'Aaron'
+    },
+    {
+        last: 'Masa',
+        first: 'Manny'
+    },
+    {
+        last: 'Tanda',
+        first: 'Tamanda'
+    }
 ];
+
+students.forEach(function(item){
+    let name = document.createElement('div');
+    name.className = 'format';
+
+    let html = `
+    <span>${item.first}</span>
+    <span>${item.last}</span>
+    <hr>
+    `;
+    name.innerHTML = html;
+    container.appendChild(name);
+})
